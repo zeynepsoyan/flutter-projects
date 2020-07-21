9d15a518e6f2873313e3a530d70657bf6cb791d8
zeynepsoyan <zeynepsoyan@gmail.com>
2020-07-21 17:40:07 +0300

    modified main.dart

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,39 +11,7 @@ class MiCardApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  color: Colors.red,
-                ),
-                Container(
-                  width: 100.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        height: 100.0,
-                        width: 100.0,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        height: 100.0,
-                        width: 100.0,
-                        color: Colors.green,
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 100.0,
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-          ),
+          child: Container(),
         ),
       ),
     );
