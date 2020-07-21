b01a09577850466d0a614cb0ab03ad67c6ffe81c
zeynepsoyan <zeynepsoyan@gmail.com>
2020-07-21 20:13:57 +0300

    created mi_card app

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,7 +11,76 @@ class MiCardApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage('images/avatar.jpg'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "zeynep hazal soyan",
+                style: TextStyle(
+                  fontFamily: "Berkshire Swash",
+                  fontSize: 35,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                "STUDENT",
+                style: TextStyle(
+                    fontFamily: "Open Sans",
+                    fontSize: 17,
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+                width: 100.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal[300]),
+                  title: Text(
+                    "+90 505 795 3515",
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: "Open Sans",
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal[300]),
+                  title: Text(
+                    "zeynepsoyan@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: "Open Sans",
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
