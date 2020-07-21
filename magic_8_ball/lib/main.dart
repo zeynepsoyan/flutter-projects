543066ce1eec2dfba07df33faee56012c1093cea
zeynepsoyan <zeynepsoyan@gmail.com>
2020-07-22 01:24:24 +0300

    modified magic ball and answers

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -33,17 +33,61 @@ class Ball extends StatefulWidget {
 
 class _BallState extends State<Ball> {
   int ballNumber = 1;
+  String answer = 'WELCOME';
+  List<String> answers = [
+    'AS I SEE IT, YES.',
+    'ASK AGAIN LATER.',
+    'BETTER NOT TELL YOU NOW.',
+    'CANNOT PREDICT NOW.',
+    'CONCENTRATE AND ASK AGAIN.',
+    'DON’T COUNT ON IT.',
+    'IT IS CERTAIN.',
+    'IT IS DECIDEDLY SO.',
+    'MOST LIKELY.',
+    'MY REPLY IS NO.',
+    'MY SOURCES SAY NO.',
+    'OUTLOOK NOT SO GOOD.',
+    'OUTLOOK GOOD.',
+    'REPLY HAZY, TRY AGAIN.',
+    'SIGNS POINT TO YES.',
+    'VERY DOUBTFUL.',
+    'WITHOUT A DOUBT.',
+    'YES.',
+    'YES – DEFINITELY.',
+    'YOU MAY RELY ON IT.'
+  ];
+
+  void replyBall() {
+    setState(() {
+      answer = answers[Random().nextInt(19)];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
-        child: Image.asset('images/ball$ballNumber.png'),
-        onPressed: () {
-          setState(() {
-            ballNumber = Random().nextInt(5) + 1;
-          });
-        },
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            Image.asset('images/magicball.png'),
+            Container(
+              height: 120.0,
+              width: 120.0,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    '$answer',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        onPressed: replyBall,
       ),
     );
   }
