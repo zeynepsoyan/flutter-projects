cf54e99d917bc730337a416c7a2eb16e13c58cf5
zeynepsoyan <zeynepsoyan@gmail.com>
2020-07-21 17:12:53 +0300

    added mi_card app

---
+++ mi_card/android/app/src/main/kotlin/neppiso/mi_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package neppiso.mi_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mi_card/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MiCardApp());
+}
+
+class MiCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    );
+  }
+}
