9d3486fca3b0eaeee13c403274f521f5e25feb07
zeynepsoyan <zeynepsoyan@gmail.com>
2020-07-21 17:25:00 +0300

    layout_challenge

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,7 +10,41 @@ class MiCardApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: 100.0,
+                        width: 100.0,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        height: 100.0,
+                        width: 100.0,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 100.0,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
